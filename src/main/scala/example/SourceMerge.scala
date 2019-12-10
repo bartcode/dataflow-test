ffd51486ba55558fdd5be6f94b4cadcde8ab99cd
Bart Hazen <bart.hazen@ing.com>
2019-12-10 12:59:37 +0100

    Add count of numbers summed to determine latest database commit.

--- src/main/scala/example/SourceMerge.scala
+++ src/main/scala/example/SourceMerge.scala
@@ -7,13 +7,12 @@ import com.spotify.scio.values.WindowOptions
 import example.objects.NumberInfo
 import example.proto.message.NumberBuffer
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions
-import org.apache.beam.sdk.transforms.windowing.{AfterPane, IntervalWindow, Repeatedly}
-import org.apache.beam.sdk.transforms.windowing.TimestampCombiner.LATEST
-import org.apache.beam.sdk.values.WindowingStrategy.AccumulationMode
-import org.joda.time.{Duration, Instant}
-import org.slf4j.{Logger, LoggerFactory}
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.{CreateDisposition, WriteDisposition}
+import org.apache.beam.sdk.transforms.windowing.TimestampCombiner.LATEST
+import org.apache.beam.sdk.transforms.windowing.{AfterPane, IntervalWindow, Repeatedly}
+import org.apache.beam.sdk.values.WindowingStrategy.AccumulationMode
+import org.joda.time.{Duration, Instant}
 
 import scala.collection.JavaConverters._
 
@@ -31,7 +30,7 @@ class SourceMerge(@transient val sc: ScioContext) extends Serializable {
       .map(x => (x.lower_bound.get, x.upper_bound.get, x.number_type.get))
       .asListSideInput
 
-    val outputs = autoInput
+    val intermediateSums = autoInput
       .union(manualInput)
       .transform("Parse objects")(_
         .map(x => NumberInfo(NumberBuffer.parseFrom(x)))
@@ -48,12 +47,16 @@ class SourceMerge(@transient val sc: ScioContext) extends Serializable {
           .withWindow[IntervalWindow]
           .map { case (score, window) => (window.end(), score.get) }
       )
+
+    val sumCounts = intermediateSums
+      .transform("Determine counted values")(
+        _.countByKey)
+
+    intermediateSums
       .transform("Sum values")(
-        _.sumByKey
-      )
+        _.sumByKey)
       .transform("Extract values")(
-        _.map(x => (x._1, x._2)) // Number length, (timestamp, sum)
-      )
+        _.map(x => (x._1, x._2))) // Number length, (timestamp, sum)
       .withSideInputs(numberInfoTable)
       .map {
         case (score, side) =>
@@ -69,12 +72,18 @@ class SourceMerge(@transient val sc: ScioContext) extends Serializable {
           )
       }
       .toSCollection
+      .transform("Combine with counts")(
+        _.map(x => (x._1._1, (x._1._2, x._2)))
+          .hashLeftJoin(sumCounts))
+      .transform("Tuple values")(
+        _.map(x => (x._1, x._2._1._1, x._2._1._2.toString, x._2._2.getOrElse(1.toLong))))
       .transform("Convert into TableRow")(
         _.map(x =>
           TableRow(Map(
-            "update_time" -> Timestamp(x._1._1),
-            "number_sum" -> x._1._2,
-            "number_type" -> x._2.toString,
+            "update_time" -> Timestamp(x._1),
+            "number_sum" -> x._2,
+            "number_count" -> x._4,
+            "number_type" -> x._3,
             "version" -> "run-1")
             .map(kv => (kv._1, kv._2))
             .toList: _*)))
@@ -126,7 +135,7 @@ object SourceMerge {
   }
 
   @BigQueryType.toTable
-  case class NumberResults(update_time: Instant, number_sum: Int, number_type: String, version: String)
+  case class NumberResults(update_time: Instant, number_sum: Int, number_count: Int, number_type: String, version: String)
 
   @BigQueryType.fromTable("playground-bart:playground.numbers")
   class NumberInfoRow
